ad188446981acf48ab2986daf1ce0363d007ffc5
hhassan1230 <hhassan@oswego.edu>
2015-09-28 00:08:12 -0400

    makes character jump and stay on screen

---
+++ Assets/scripts/robotMove.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class robotMove : MonoBehaviour {
+	private Rigidbody2D playerRigidbody;
+	private int currentScore = 0;
+//	public Text scoreTextUI;
+	private Vector2 myScreenPosition;
+	private float countdown = 10;
+	private bool isPlayerAlive = true;
+	// Use this for initialization
+	void Start () {
+		isPlayerAlive = true;
+		currentScore = 0;
+		countdown = 10;
+		playerRigidbody = this.gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			print("I AM SPACEBAR");
+			
+			playerRigidbody.velocity = new Vector2(0,0);
+			
+			Vector2 tempForce = new Vector2(0,200);
+			
+			playerRigidbody.AddForce(tempForce);
+		};
+		if (isPlayerAlive) {
+			checkPlayerPosition ();
+		};
+	}
+	void checkPlayerPosition () {
+		myScreenPosition = Camera.main.WorldToScreenPoint(transform.position);
+		
+		if (myScreenPosition.y > Screen.height || myScreenPosition.y < 0 || myScreenPosition.x < 0 || myScreenPosition.x > Screen.width) {
+			Die();
+		};
+	}
+	void Die(){
+		//		InvokeRepeating ("countdownDec", 1, 1);
+		isPlayerAlive = false;
+//		scoreTextUI.text = "Game Over";
+		GameOver ();
+	}
+	void GameOver(){
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
